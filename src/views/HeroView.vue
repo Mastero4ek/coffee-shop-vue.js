a346e2c0a91202f63ad738ca199b237be7ecd905
Chirkov Slava <slavachirkov92@gmail.com>
2023-05-22 17:41:14 +0300

    data links, productCards

--- src/views/HeroView.vue
+++ src/views/HeroView.vue
@@ -76,5 +76,29 @@ import ProductCard from "@/components/ProductCard.vue";
 
 export default {
   components: { NavBarComponent, ProductCard },
+  data() {
+    return {
+      productCards: [
+        {
+          id: 0,
+          image: "coffee-1.jpg",
+          text: "Solimo Coffee Beans 2kg",
+          price: "10.73$",
+        },
+        {
+          id: 1,
+          image: "coffee-2.jpg",
+          text: "Presto Coffee Beans 1kg",
+          price: "15.99$",
+        },
+        {
+          id: 2,
+          image: "coffee-3.jpg",
+          text: "AROMISTICO Coffee 1kg",
+          price: "6.99$",
+        },
+      ],
+    };
+  },
 };
 </script>
--- src/views/OurCoffeeView.vue
+++ src/views/OurCoffeeView.vue
@@ -89,5 +89,53 @@ import ProductCard from "@/components/ProductCard.vue";
 
 export default {
   components: { NavBarComponent, ProductCard },
+  data() {
+    return {
+      productCards: [
+        {
+          id: 0,
+          image: "coffee-1.jpg",
+          text: "Solimo Coffee Beans 2kg",
+          country: "Brazil",
+          price: "10.73$",
+        },
+        {
+          id: 1,
+          image: "coffee-2.jpg",
+          text: "Presto Coffee Beans 1kg",
+          country: "Brazil",
+          price: "15.99$",
+        },
+        {
+          id: 2,
+          image: "coffee-3.jpg",
+          text: "AROMISTICO Coffee 1kg",
+          country: "Kenya",
+          price: "6.99$",
+        },
+        {
+          id: 3,
+          image: "coffee-1.jpg",
+          text: "Solimo Coffee Beans 2kg",
+          country: "Columbia",
+          price: "10.73$",
+        },
+        {
+          id: 4,
+          image: "coffee-2.jpg",
+          text: "Presto Coffee Beans 1kg",
+          country: "Brazil",
+          price: "15.99$",
+        },
+        {
+          id: 5,
+          image: "coffee-3.jpg",
+          text: "AROMISTICO Coffee 1kg",
+          country: "Kenya",
+          price: "6.99$",
+        },
+      ],
+    };
+  },
 };
 </script>
